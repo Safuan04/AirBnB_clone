fb697df8bcf7d62af14be4f4ebd82bd8d2cb94e6
Safuan04 <elmai.safuan@gmail.com>
2023-08-13 16:02:20 -0700

    BaseModel

--- __init__.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python3
-"""Defining a variable storage to create a unique
-FileStorage instance for the AirBnB Application
-"""
-from models.engine.file_storage import FileStorage
-
-
-storage = FileStorage()
-storage.reload()
