5c87e13e3e7a74309b52481e00beedce0922efb3
Elbadil <elbaliadil@gmail.com>
2023-08-14 00:07:39 +0100

    State and User Class

---
+++ models/state.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Defining a class called State"""
+from models.base_model import BaseModel
+
+
+class State(BaseModel):
+    """class State that inherits from BaseModel"""
+
+    name = ""
