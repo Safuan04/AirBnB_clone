25ee67ae8225eda4f24f8a963bb9d0de85894573
Elbadil <elbaliadil@gmail.com>
2023-08-14 00:04:07 +0100

    Amenity Class

---
+++ models/amenity.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Defining a class called Amenity"""
+from models.base_model import BaseModel
+
+
+class Amenity(BaseModel):
+    """class Amenity that inherits from BaseModel"""
+
+    name = ""
